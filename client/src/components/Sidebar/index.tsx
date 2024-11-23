a9718821ce2471f1b62237627e99963cd6af225b
ZBoyukliev <zarkoboyukliev@gmail.com>
2024-11-23 23:17:18 +0200

    style a sidebar

--- client/src/components/Sidebar/index.tsx
+++ client/src/components/Sidebar/index.tsx
@@ -1,4 +1,5 @@
 "use client"
+import { LockIcon } from 'lucide-react';
 import Image from 'next/image';
 import React, { useState } from 'react'
 
@@ -28,8 +29,16 @@ const Sidebar = () => {
                         width={40}
                         height={40}
                     />
+                    <div>
+                        <h3 className="text-md font-bold tracking-wide dark:text-gray-200">
+                            ZARKO TEAM
+                        </h3>
+                        <div className="mt-1 flex items-start gap-2">
+                            <LockIcon className="mt-[0.1rem] h-3 w-3 text-gray-500 dark:text-gray-400" />
+                            <p className="text-xs text-gray-500">Private</p>
+                        </div>
+                    </div>
                 </div>
-
             </div>
         </div>
     )
