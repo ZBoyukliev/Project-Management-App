f6e9a8d28fc43944d586bc114944249870d19e8b
ZBoyukliev <zarkoboyukliev@gmail.com>
2024-11-29 11:09:59 +0200

    create a dashboard wrapper and layout

--- client/src/app/dashboardWrapper.tsx
+++ client/src/app/dashboardWrapper.tsx
@@ -1,8 +1,11 @@
+"use client"
+
 import Navbar from '@/components/NavBar';
 import Sidebar from '@/components/Sidebar';
 import React from 'react'
+import StoreProvider from './redux';
 
-const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
+const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex min-h-screen w-full bg-gray-50 text-gray-900">
       <Sidebar />
@@ -16,5 +19,13 @@ const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
     </div>
   )
 }
+
+const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <StoreProvider>
+        <DashboardLayout>{children}</DashboardLayout>
+    </StoreProvider>
+  );
+};
 
 export default DashboardWrapper;
