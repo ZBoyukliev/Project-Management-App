5b4ede5a20a40962ab517df756ac1414beed5973
ZBoyukliev <zarkoboyukliev@gmail.com>
2024-12-04 22:12:18 +0200

    create a home side-bar link

--- client/src/components/Sidebar/index.tsx
+++ client/src/components/Sidebar/index.tsx
@@ -1,5 +1,6 @@
 "use client"
-import { LockIcon, LucideIcon } from 'lucide-react';
+import { useAppDispatch, useAppSelector } from '@/app/redux';
+import { Home, LockIcon, LucideIcon } from 'lucide-react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
@@ -9,6 +10,11 @@ const Sidebar = () => {
 
     const [showProjects, setShowProjects] = useState(true);
     const [showPriority, setShowPriority] = useState(true);
+
+    const dispatch = useAppDispatch();
+    const isSidebarCollapsed = useAppSelector(
+        (state) => state.global.isSidebarCollapsed,
+    );
 
     const sidebarClassNames = `fixed flex flex-col h-[100%] justify-between shadow-xl
     transition-all duration-300 h-full z-40 dark:bg-black overflow-y-auto bg-white "w-64"
@@ -41,6 +47,9 @@ const Sidebar = () => {
                         </div>
                     </div>
                 </div>
+                <nav className="z-10 w-full">
+                    <SidebarLink icon={Home} label="Home" href="/" />
+                </nav>
             </div>
         </div>
     )
@@ -50,31 +59,30 @@ interface SidebarLinkProps {
     href: string;
     icon: LucideIcon;
     label: string;
-  }
-  
-  const SidebarLink = ({ href, icon: Icon, label }: SidebarLinkProps) => {
+}
+
+const SidebarLink = ({ href, icon: Icon, label }: SidebarLinkProps) => {
     const pathname = usePathname();
     const isActive =
-      pathname === href || (pathname === "/" && href === "/dashboard");
-  
+        pathname === href || (pathname === "/" && href === "/dashboard");
+
     return (
-      <Link href={href} className="w-full">
-        <div
-          className={`relative flex cursor-pointer items-center gap-3 transition-colors hover:bg-gray-100 dark:bg-black dark:hover:bg-gray-700 ${
-            isActive ? "bg-gray-100 text-white dark:bg-gray-600" : ""
-          } justify-start px-8 py-3`}
-        >
-          {isActive && (
-            <div className="absolute left-0 top-0 h-[100%] w-[5px] bg-blue-200" />
-          )}
-  
-          <Icon className="h-6 w-6 text-gray-800 dark:text-gray-100" />
-          <span className={`font-medium text-gray-800 dark:text-gray-100`}>
-            {label}
-          </span>
-        </div>
-      </Link>
+        <Link href={href} className="w-full">
+            <div
+                className={`relative flex cursor-pointer items-center gap-3 transition-colors hover:bg-gray-100 dark:bg-black dark:hover:bg-gray-700 ${isActive ? "bg-gray-100 text-white dark:bg-gray-600" : ""
+                    } justify-start px-8 py-3`}
+            >
+                {isActive && (
+                    <div className="absolute left-0 top-0 h-[100%] w-[5px] bg-blue-200" />
+                )}
+
+                <Icon className="h-6 w-6 text-gray-800 dark:text-gray-100" />
+                <span className={`font-medium text-gray-800 dark:text-gray-100`}>
+                    {label}
+                </span>
+            </div>
+        </Link>
     );
-  };
+};
 
 export default Sidebar
