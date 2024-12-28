6be96790bb0ab694dd373841cbbedfb993727bdc
ZBoyukliev <zarkoboyukliev@gmail.com>
2024-12-28 11:42:36 +0200

    create a prject interface in api-state

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -1,5 +1,13 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
+export interface Project {
+    id: number;
+    name: string;
+    description?: string;
+    startDate?: string;
+    endDate?: string;
+  }
+
 export const api = createApi({
     baseQuery: fetchBaseQuery({
         baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL,
