fa295062117154cab52912277a584c9ac226e54c
ZBoyukliev <zarkoboyukliev@gmail.com>
2024-12-28 11:48:57 +0200

    create a task interface

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -8,6 +8,36 @@ export interface Project {
     endDate?: string;
   }
 
+  export enum Priority {
+    Urgent = "Urgent",
+    High = "High",
+    Medium = "Medium",
+    Low = "Low",
+    Backlog = "Backlog",
+  }
+  
+  export enum Status {
+    ToDo = "To Do",
+    WorkInProgress = "Work In Progress",
+    UnderReview = "Under Review",
+    Completed = "Completed",
+  }
+
+  export interface Task {
+    id: number;
+    title: string;
+    description?: string;
+    status?: Status;
+    priority?: Priority;
+    tags?: string;
+    startDate?: string;
+    dueDate?: string;
+    points?: number;
+    projectId: number;
+    authorUserId?: number;
+    assignedUserId?: number;
+  }
+
 export const api = createApi({
     baseQuery: fetchBaseQuery({
         baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL,
