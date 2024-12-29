7edccbaa1738d390ac79d8d8f312b3072c0d4d9c
ZBoyukliev <zarkoboyukliev@gmail.com>
2024-12-29 11:34:30 +0200

    create a get and create project fetch data

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -65,8 +65,24 @@ export const api = createApi({
         baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL,
     }),
     reducerPath: "api",
-    tagTypes: [],
-    endpoints: (build) => ({})
+    tagTypes: ["Projects", "Tasks"],
+    endpoints: (build) => ({
+        getProjects: build.query<Project[], void>({
+            query: () => "projects",
+            providesTags: ["Projects"],
+          }),
+          createProject: build.mutation<Project, Partial<Project>>({
+            query: (project) => ({
+              url: "projects",
+              method: "POST",
+              body: project,
+            }),
+            invalidatesTags: ["Projects"],
+          }),
+    })
 })
 
-export const { } = api
+export const {
+    useGetProjectsQuery,
+    useCreateProjectMutation
+ } = api
