e53a37ea3838bb53c149bb9a0e8fe12b5ca23765
ZBoyukliev <zarkoboyukliev@gmail.com>
2024-12-30 11:30:46 +0200

    create a project header

---
+++ client/src/app/projects/ProjectHeader.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+type Props = {
+    activeTab: string;
+    setActiveTab: (tabName: string) => void;
+  };
+  
+
+const ProjectHeader = ({ activeTab, setActiveTab }: Props) => {
+  return (
+    <div>ProjectHeader</div>
+  )
+}
+
+export default ProjectHeader
