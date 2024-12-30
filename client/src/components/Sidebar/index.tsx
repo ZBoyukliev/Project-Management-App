9d9d48ceb0ce9413071c81c561f71d014929828e
ZBoyukliev <zarkoboyukliev@gmail.com>
2024-12-30 11:43:47 +0200

    fetch the projects in sidebar

--- client/src/components/Sidebar/index.tsx
+++ client/src/components/Sidebar/index.tsx
@@ -1,6 +1,7 @@
 "use client"
 import { useAppDispatch, useAppSelector } from '@/app/redux';
 import { setIsSidebarCollapsed } from '@/state';
+import { useGetProjectsQuery } from '@/state/api';
 import { Briefcase, ChevronDown, ChevronUp, Home, LockIcon, LucideIcon, Search, Settings, User, Users, X } from 'lucide-react';
 import Image from 'next/image';
 import Link from 'next/link';
@@ -12,6 +13,7 @@ const Sidebar = () => {
     const [showProjects, setShowProjects] = useState(true);
     const [showPriority, setShowPriority] = useState(true);
 
+    const { data: projects } = useGetProjectsQuery();
     const dispatch = useAppDispatch();
     const isSidebarCollapsed = useAppSelector(
         (state) => state.global.isSidebarCollapsed,
@@ -78,6 +80,15 @@ const Sidebar = () => {
                         <ChevronDown className="h-5 w-5" />
                     )}
                 </button>
+                {showProjects &&
+                    projects?.map((project) => (
+                        <SidebarLink
+                            key={project.id}
+                            icon={Briefcase}
+                            label={project.name}
+                            href={`/projects/${project.id}`}
+                        />
+                    ))}
                 <button
                     onClick={() => setShowPriority((prev) => !prev)}
                     className="flex w-full items-center justify-between px-8 py-3 text-gray-500"
