f01e260dad90553ada3d2d6e9ab522b95d3ffb24
ZBoyukliev <zarkoboyukliev@gmail.com>
2025-01-17 15:07:38 +0200

    import board view in board page

--- client/src/app/projects/[id]/page.tsx
+++ client/src/app/projects/[id]/page.tsx
@@ -2,6 +2,7 @@
 
 import React, { useState } from "react";
 import ProjectHeader from "@/app/projects/ProjectHeader";
+import Board from "../BoardView";
 
 
 type Props = {
@@ -16,6 +17,9 @@ const Project = ({ params }: Props) => {
   return (
     <div>
       <ProjectHeader activeTab={activeTab} setActiveTab={setActiveTab} />
+      {activeTab === "Board" && (
+        <Board id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen} />
+      )}
     </div>
   );
 };
