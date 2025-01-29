356d21d35847f6bfbdd101a5eafbe6ec03d896b7
ZBoyukliev <zarkoboyukliev@gmail.com>
2025-01-29 19:25:08 +0200

    import search routes in index file

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,7 +6,8 @@ import helmet from "helmet";
 import morgan from "morgan";
 /* ROUTE IMPORTS */
 import projectRoutes from "./routes/projectRoutes";
-import taskRoutes from "./routes/taskRoutes"
+import taskRoutes from "./routes/taskRoutes";
+import searchRoutes from "./routes/searchRoutes";
 
 /* CONFIGURATIONS */
 dotenv.config();
@@ -26,6 +27,7 @@ app.get("/", (req, res) => {
 
 app.use("/projects", projectRoutes);
 app.use("/tasks", taskRoutes);
+app.use("/search", searchRoutes);
 
 /* SERVER */
 const port = Number(process.env.PORT) || 3000;
