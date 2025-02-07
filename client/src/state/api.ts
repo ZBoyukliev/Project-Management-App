57b074b311fa3b625f1f89dd389de3574ec074b1
ZBoyukliev <zarkoboyukliev@gmail.com>
2025-02-07 19:22:38 +0200

    add get tasks by user api

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -99,6 +99,13 @@ export const api = createApi({
                     ? result.map(({ id }) => ({ type: "Tasks" as const, id }))
                     : [{ type: "Tasks" as const }],
         }),
+        getTasksByUser: build.query<Task[], number>({
+            query: (userId) => `tasks/user/${userId}`,
+            providesTags: (result, error, userId) =>
+                result
+                    ? result.map(({ id }) => ({ type: "Tasks", id }))
+                    : [{ type: "Tasks", id: userId }],
+        }),
         createTask: build.mutation<Task, Partial<Task>>({
             query: (task) => ({
                 url: "tasks",
@@ -139,5 +146,6 @@ export const {
     useUpdateTaskStatusMutation,
     useSearchQuery,
     useGetUsersQuery,
-    useGetTeamsQuery
+    useGetTeamsQuery,
+    useGetTasksByUserQuery
 } = api
