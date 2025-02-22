78e2e0244f9a0c5f8d905f482d035b9aab176b46
ZBoyukliev <zarkoboyukliev@gmail.com>
2025-02-22 17:31:55 +0200

    common changes

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     apps: [
       {
-        name: "project-management",
+        name: "App-project-management",
         script: "npm",
         args: "run dev",
         env: {
